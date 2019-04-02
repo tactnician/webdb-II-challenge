df9748487c2131eed0b3e37d2a32d912553f4b2b
tactnician <tactnician@gmail.com>
2019-04-02 17:39:14 -0400

    post route working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const zooRoute = require('./zoos/zoos-router');
 
 const server = express();
 
@@ -7,8 +8,21 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.use( '/api/zoos', zooRoute);
 
 const port = 3300;
+
+//sanity check 
+server.get("/", (req, res)=>{
+  console.log("sane in the membrane")
+  res.send(`
+  <div style="background-image: url('https://i.ytimg.com/vi/cwxDIrF494g/maxresdefault.jpg'); background-size: cover; height: 700px; ">
+    <h2 style="color:white;"> Welcome to the Zoo </h2>
+  </div>
+  `)
+});
+
+
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const knex = require('knex');
 
-cons knexConfig = {
+const knexConfig = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
@@ -13,24 +13,42 @@ const db = knex(knexConfig);
 
 
 
-router.get('/', async (req, res) => {
+// router.get('/', async (req, res) => {
+//     try {
+//         const zooList = await db('zoos');
+//         res.send(200).json(zooList);
+//     } catch (error) {
+//         res.send(500).json({message:`unable to fetch zoos ${error}`});
+//     }
+// });
 
+// router.get('./:id' async (req, res) =>{
+
+// });
+
+router.post('/', (req, res) => {
+    db('zoos')
+        .insert(req.body)
+        .then(ids => {
+            const [id] = ids
+            db('zoos')
+                .where({id})
+                .first()
+                .then(zoo => {
+                    res.status(200).json(zoo)
+                })
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
 });
 
-router.get('./id' async (req, res) =>{
+// router.put('/:id', (req, res) => {
 
-});
+// })
 
-router.post('/', async (req, res) => {
+// router.delete('/:id', (req, res) => {
 
-});
-
-router.put('/:id', (req, res) => {
-
-})
-
-router.delete('/:id', (req, res) => {
-
-})
+// })
 
 module.exports = router;
